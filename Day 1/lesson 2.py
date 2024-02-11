c67b44c36c47372d4274ee41374cb39d6be30f9a
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-02-11 16:02:11 +0400

    first commit

---
+++ Day 1/lesson 2.py
@@ -0,0 +1,7 @@
+name = "giorgi"
+surname = "davitashvili"
+age = 15
+height = 175
+
+
+print("me var" + " " + name + " " + "cemi gvaria" + " " + surname + ", " "cemi simarlea" + " " + str(height) +  "cm" + " " + "da" + " " + str(age) + " " + "wlis" + " "  + "var")
