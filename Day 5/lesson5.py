2900d554b236b8fe4f51ce93fa8e52eb1441a935
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-03-04 17:44:39 +0400

    fifth commit

---
+++ Day 5/lesson5.py
@@ -0,0 +1,44 @@
+num1 = type(input("Please enter first number: "))
+num2 = type(input("Please enter second number: "))
+
+
+
+print(59 / 25)
+print(59 // 25)
+print(59 % 25)
+
+print(28 / 5)
+print(28 // 5)
+print(28 % 5)
+
+print(350 / 50)
+print(350 // 50)
+print(350 % 50)
+
+print(35.90 / 15.45)
+print(35.90 // 15.45)
+print(35.90 % 15.45)
+
+print(1625 / 25)
+print(1625 // 25)
+print(1625 % 25)
+
+print(1.2 / 0.6)
+print(1.2 // 0.6)
+print(1.2 % 0.6)
+
+print(810 / 90)
+print(810 // 90)
+print(810 % 90)
+
+print(45560 / 15)
+print(45560 // 15)
+print(45560 % 15)
+
+print(1677 / 349)
+print(1677 // 349)
+print(1677 % 349)
+
+print(420 / 69)
+print(420 // 69)
+print(420 % 69)
