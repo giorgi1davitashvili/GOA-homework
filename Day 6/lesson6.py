2262b2f5bee0e26a08c7e89fd035b104a0fa7b4d
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-03-11 18:22:15 +0400

    sixth commit

---
+++ Day 6/lesson6.py
@@ -0,0 +1,74 @@
+number1 = 500
+number2 = 500.0    #ან 0.500 ვერ გავიგე
+
+print(number1 == number2)
+
+
+
+print(210 > 93)
+print(439 > 561)
+print(450 > 150)
+print(921 > 920)
+print(57 > 34)
+
+print(320 >= 317)
+print(285 >= 286)
+print(219 >= 126)
+print(18 >= 391)
+print(35 >= 28)
+
+
+print(497 < 285)
+print(100 < 174)
+print(853 < 1002)
+print(492.4 < 399.8)
+print(8 < 403)
+
+print(492 <= 547.9)
+print(842.1 <= 932)
+print(767 <= 324)
+print(5483 <= 5994.31)
+print(56 <= 78)
+
+print(594 == 594)
+print(431 == 432)
+print(59 == 59)
+print(0.139 == 0.145)
+print(292 == 292)
+
+print(493 != 392)
+print(12 != 12)
+print(584 != 231)
+print(40 != 90)
+print(29146 != 43920)
+
+
+number_1 = 4.99
+number_2 = 24.99
+
+print(number_1 > number_2)
+print(number_1 >= number_2)
+print(number_1 < number_2)
+print(number_1 <= number_2)
+print(number_1 == number_2)
+print(number_1 != number_2)
+
+
+num1 = 110
+
+print(num1 * 10 > num1 * 10)
+print(num1 * 10 >= num1 * 10)
+print(num1 * 10 < num1 * 10)
+print(num1 <= num1 * 10)
+print(num1 * 10 == num1 * 10)
+print(num1 * 10 != num1 * 10)
+
+
+number_3 = input(30)
+number_4 = input(15.0)
+
+print(number_3 == number_4)
+print(number_3 != number_4)
+
+
+
