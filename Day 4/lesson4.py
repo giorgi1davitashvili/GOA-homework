011c7425a5a764d47c9b11551ba1d1ea4fe2b42a
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-03-16 19:11:25 +0400

    fourth commit

---
+++ Day 4/lesson4.py
@@ -0,0 +1,19 @@
+number1 = int(input("Please enter first number: "))
+number2 = int(input("Please enter second number: "))
+
+print(number1 + number2)
+
+
+name = input("Enter name here: ")
+age = int(input("enter age here: "))
+
+print("How far do you wish to time travel?")
+time_travel = int(input("Enter answer here: "))
+
+print("Your future self's age will be..")
+
+print(age + time_travel)
+
+
+
+
