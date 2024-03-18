492bf04f85bf91ffa7ba09ab96b9c87aab6d5fa4
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-03-18 22:44:36 +0400

    seventh commit

---
+++ Day 7/classwork/classwork.py
@@ -0,0 +1,9 @@
+number1 = int(input("Please enter number here: "))
+
+print(number1 >= 0 and number1 < 5)
+
+
+
+number2 = int(input("Please enter number here: "))
+
+print(number2 > 10)
---
+++ Day 7/lesson7.py
@@ -0,0 +1,28 @@
+
+
+print("Whats the temperature?")
+
+temp = int(input("Please enter here: "))
+
+print((temp * 1.8) + 32)
+
+
+print("How old are you?")
+
+age = int(input("Please enter from 12-20 here: "))
+
+print(age == 20)
+print(age == 12)
+print(age != 20)
+print(age != 12)
+
+
+
+num1 = int(input("Enter number here: "))
+
+
+print(num1 > 100)
+print(num1 / 9)
+print(num1 // 9)
+print(num1 % 9)
+
