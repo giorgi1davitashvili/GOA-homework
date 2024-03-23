740781a852973a972d1551e7a5708a11cd29d180
giorgi1davitashvili <2213122gio2g@gmail.com>
2024-03-23 17:46:58 +0400

    sixth commit

---
+++ Day 6/homework/lesson6.py
@@ -0,0 +1,23 @@
+number1 = 500
+number2 = 500.0    #ან 0.500 ვერ გავიგე
+
+print(number1 == number2)               #\
+
+
+
+
+
+
+
+
+print_number1 = input(210)
+
+print(print_number1 > print_number1)
+
+
+
+
+
+
+
+
